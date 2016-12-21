2510b88e4afaa590adcff1ed99c785d99d0159c1
shift02 <shift02ss@gmail.com>
2016-12-22 01:25:14 +0900

    変更：info

--- src/main/java/shift/seasonapi/package-info.java
+++ src/main/java/shift/seasonapi/package-info.java
@@ -1,4 +1,4 @@
-@API(apiVersion = "1.0.0", owner = "sextiarysector3|core", provides = "seasonapi")
+@API(apiVersion = "1.0.0", owner = "sextiarysector3api|core", provides = "seasonapi")
 package shift.seasonapi;
 
 import net.minecraftforge.fml.common.API;
